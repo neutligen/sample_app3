d077cabc3967b45fa7dc08bf2448b03ac5e0593e
neutligen <neutligen@gmail.com>
2015-08-27 09:55:20 +0900

    Finish layout and routes 2

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,12 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+#以下は5.6.3の演習によって、spec/helpers/application_helper_spec.rbを作成し、それを上記コードでincludeしたのでコメントアウトした。
+
+#def full_title(page_title)
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
